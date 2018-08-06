80f955d1779bfea96a0e3e6f42edf50e07f672f1
Vladimir Boliev <voff.web@gmail.com>
2018-08-06 13:37:24 +0300

    Remove manual error handling

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -11,15 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * @param string $mess
-     * @param int    $code
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function throwError(string $mess, int $code): JsonResponse
-    {
-        return response()->json(['message' => $mess], $code);
-    }
 }
